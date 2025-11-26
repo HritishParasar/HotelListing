c5d29114d37f41764ab45680da80102b2481764d
Hritish <hritishparasar07@gmail.com>
2025-11-26 19:14:48 +0530

    Added DbContext and Perform Migration

---
+++ HotelListing.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HotelListing.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Reviews> Reviews { get; set; }
+    }
+}
