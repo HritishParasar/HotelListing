ba396c2c03917a3cad1f6f1f65efb876b869e87b
Hritish <hritishparasar07@gmail.com>
2025-11-26 18:54:13 +0530

    Add project files.

---
+++ HotelListing.API/Models/Reviews.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelListing.API.Models
+{
+    public class Reviews
+    {
+        public int Id { get; set; }
+        public string? ReviewerName { get; set; }
+        public double? Rating { get; set; }
+        public string? ReviewText { get; set; }
+        
+        public int HotelId { get; set; }
+        public Hotel? Hotel { get; set; }
+    }
+}
